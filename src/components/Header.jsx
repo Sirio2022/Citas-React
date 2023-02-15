f8416cfe2273d22eed9b45171503766b583bbac3
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2023-02-15 11:27:37 -0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 function Header() {
   return (
     <header>
-      <h1>Administrador de Pacientes</h1>
+      <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
+        Seguimiento Pacientes{' '}
+        <span className="text-indigo-500">Veterinaria</span>
+      </h1>
     </header>
   );
 }
