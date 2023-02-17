4c5a25dcea58384246b54f3c176b5e73607671f7
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2023-02-17 11:21:00 -0500

    Quedó brutal!
    Lista esta aplicación.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Formulario from './components/Formulario';
 import Header from './components/Header';
 import ListadoPacientes from './components/ListadoPacientes';
@@ -6,6 +6,18 @@ import ListadoPacientes from './components/ListadoPacientes';
 function App() {
   const [pacientes, setPacientes] = useState([]);
   const [paciente, setPaciente] = useState({});
+
+  useEffect(() => {
+    const obtenerLS = () => {
+      const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+      setPacientes(pacientesLS);
+    };
+    obtenerLS();
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('pacientes', JSON.stringify(pacientes));
+  }, [pacientes]);
 
   const eliminarPaciente = (id) => {
     const pacientesFiltrados = pacientes.filter(
